328ef1bcbe4300de462817ced8d7bf885ddfe96d
vinnie777 <mtarassov@gmail.com>
2015-02-26 13:18:59 +0100

    t1 tenant resources files dump 1424953104

---
+++ files/mik_lib/mik_lib/mik_js.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Mikhail on 06/10/14.
+ */
+
+var mik_test_function = function() {
+    return 'Hello from external function!';
+}
---
+++ files/test/mik_js_sub_folder.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Mikhail on 06/10/14.
+ */
+
+var mik_test_function_sub_folder = function() {
+    console.log('Hello from external function in sub folder!');
+}
